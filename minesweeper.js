52f77096ed7a8c41f5cf8c55a524928ff2c38102
BryanPetko <79470625+TrilliumBlaise@users.noreply.github.com>
2022-07-12 10:45:18 -0400

    Add files via upload

---
+++ minesweeper.js
@@ -0,0 +1,117 @@
+const game = {
+  gridSize: {
+    easy: [9, 9],
+    medium: [16, 16],
+    hard: [30, 16],
+  },
+  minesAvailable: {
+    easy: 10,
+    medium: 40,
+    hard: 99,
+  },
+}
+export function createTiles(mode) {
+  const [numberRows, numberColumns] = game.gridSize[mode]
+
+  const tiles = []
+  for (let x = 0; x < numberRows; x++) {
+    const row = []
+    for (let y = 0; y < numberColumns; y++) {
+      const tile = {
+        x,
+        y,
+        mine: false,
+        status: 'closed',
+        element: createBox(x, y, tiles),
+      }
+      row.push(tile)
+    }
+    tiles.push(row)
+  }
+  setMines(mode, tiles)
+  //   testDistribution(tiles)
+  return tiles
+}
+export function getAllMines(board) {
+  return board.flat().filter(t => t.mine)
+}
+export function setNumberOfMines(mode) {
+  return game.minesAvailable[mode]
+}
+
+function setMines(mode, tiles) {
+  const mines = []
+
+  while (mines.length < game.minesAvailable[mode]) {
+    const x = Math.floor(Math.random() * tiles.length)
+    const y = Math.floor(Math.random() * tiles[0].length)
+
+    if (!mines.some(mine => mine[0] === x && mine[1] === y)) mines.push([x, y])
+  }
+  mines.forEach(mine => {
+    tiles[mine[0]][mine[1]].mine = true
+  })
+}
+function createBox(x, y, board) {
+  const tileElement = document.createElement('div')
+  tileElement.classList.add('box')
+  tileElement.setAttribute('data-index', `${x},${y}`)
+  tileElement.addEventListener('click', e => {
+    if (e.shiftKey) {
+      e.target.classList.toggle('is-mine')
+      return
+    }
+    const tile = board[x][y]
+    if (isBomb(tile, board)) {
+      const mines = getAllMines(board)
+      mines.forEach(mine => mine.element.classList.add('mine', 'open-box'))
+      tile.element.setAttribute('data-value', 'X')
+      tile.element.textContent = 'X'
+    } else {
+      revealTile(board, tile)
+    }
+  })
+
+  return tileElement
+}
+
+function isBomb({ x, y }, board) {
+  if (board[x][y].mine === true) return true
+  return false
+}
+
+function revealTile(board, tile) {
+  if (tile.status === 'open') return
+  if (tile.mine === true) return
+  tile.status = 'open'
+  const adjacentTiles = nearbyTiles(board, tile)
+  const mines = adjacentTiles.filter(t => t.mine)
+  if (mines.length === 0) {
+    adjacentTiles.forEach(revealTile.bind(null, board))
+    adjacentTiles.forEach(tile => {
+      tile.element.classList.add('open-box')
+    })
+  } else {
+    tile.element.textContent = mines.length
+    tile.element.dataset.value = mines.length
+    tile.element.classList.add('open-box')
+  }
+}
+
+function nearbyTiles(board, { x, y }) {
+  const tiles = []
+
+  for (let xOffset = -1; xOffset <= 1; xOffset++) {
+    for (let yOffset = -1; yOffset <= 1; yOffset++) {
+      const tile = board[x + xOffset]?.[y + yOffset]
+      if (tile) tiles.push(tile)
+    }
+  }
+  return tiles
+}
+
+function testDistribution(tiles) {
+  tiles.flat().forEach(tile => {
+    if (tile.mine === true) tile.element.textContent = 'T'
+  })
+}
---
+++ script.js
@@ -0,0 +1,111 @@
+import { createTiles, getAllMines, setNumberOfMines } from './minesweeper.js'
+
+const playArea = document.querySelector('.play-area')
+const difficultyMenu = document.querySelector('.pick-difficulty')
+
+const game = {
+  board: [],
+  mode: sessionStorage.getItem('mode') || 'easy',
+  time: {
+    hundreds: 0,
+    tens: 0,
+    ones: 0,
+  },
+  timerInterval: undefined,
+}
+document.querySelector('.difficulty').addEventListener('click', () => {
+  difficultyMenu.classList.toggle('closed')
+})
+document
+  .querySelector('.pick-difficulty')
+  .querySelectorAll('button')
+  .forEach(button => {
+    button.addEventListener('click', e => {
+      difficultyMenu.classList.add('closed')
+      const target = e.target.closest('button')
+      changeDifficult(target)
+    })
+  })
+playArea.addEventListener('click', () => {
+  if (game.timerInterval == undefined) game.timerInterval = setInterval(startTimer, 1000)
+  displayMinesLeft(game.board)
+  checkWin()
+})
+document.querySelector('.reset').addEventListener('click', e => {
+  if (game.timerInterval != undefined) {
+    clearInterval(game.timerInterval)
+    game.timerInterval = undefined
+  }
+  e.target.style.backgroundImage = `url('./images/smile.svg')`
+  game.time = { hundreds: 0, tens: 0, ones: 0 }
+  displayTime()
+  setUpPlayArea()
+})
+function startTimer() {
+  game.time.ones++
+  if (game.time.ones > 9) {
+    game.time.ones = 0
+    game.time.tens++
+  }
+  if (game.time.tens > 9) {
+    game.time.tens = 0
+    game.time.hundreds++
+  }
+  displayTime()
+}
+
+function displayTime() {
+  const timer = document.querySelector('.timer')
+  timer.children[0].textContent = game.time.hundreds
+  timer.children[1].textContent = game.time.tens
+  timer.children[2].textContent = game.time.ones
+}
+function setUpPlayArea() {
+  playArea.innerHTML = ''
+  playArea.classList.add(game.mode)
+  game.board = createTiles(game.mode)
+  game.board.forEach(row => {
+    row.forEach(tile => {
+      playArea.append(tile.element)
+    })
+  })
+  displayMinesLeft(game.board)
+}
+
+function displayMinesLeft(board) {
+  const minesLeftElement = document.querySelector('.mines-left')
+  const numberOfMines = setNumberOfMines(game.mode) - board.flat().filter(tile => tile.element.classList.contains('is-mine')).length
+  const [tens, ones] = numberOfMines.toString().split('')
+  minesLeftElement.children[0].textContent = ones != undefined ? tens : 0
+  minesLeftElement.children[1].textContent = ones != undefined ? ones : tens
+}
+function changeDifficult(target) {
+  playArea.classList.remove(playArea.classList[1])
+  game.mode = target.classList
+  sessionStorage.setItem('mode', game.mode)
+  setUpPlayArea()
+}
+
+function checkWin() {
+  const board = [...playArea.querySelectorAll('.box')]
+  const mines = getAllMines(game.board)
+  if (board.filter(tile => tile.textContent === 'X').length === 1) {
+    clearInterval(game.timerInterval)
+    game.timerInterval = undefined
+    document.querySelector('.reset').style.backgroundImage = `url('./images/dead.svg')`
+    playArea.style.pointerEvents = 'none'
+  }
+  if (mines.filter(mine => mine.element.classList.contains('is-mine')).length === mines.length) {
+    console.log('hello')
+    playArea.style.pointerEvents = 'none'
+    const congratsMessage = document.createElement('div')
+    congratsMessage.classList.add('congrats')
+    congratsMessage.innerHTML = `Congratulations!<br>You win!`
+    playArea.append(congratsMessage)
+    clearInterval(game.timerInterval)
+    game.timerInterval = undefined
+    startConfetti()
+  }
+}
+
+setUpPlayArea()
